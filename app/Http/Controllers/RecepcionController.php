28ca28345d4dd81c9c01aebb4f39fab2dfaa8715
dtsi-unidad-desarrollo <dtsi.unidad.programadores@gmail.com>
2025-11-26 12:25:43 -0400

    fix: configurando consulta db_terepaima

--- app/Http/Controllers/RecepcionController.php
+++ app/Http/Controllers/RecepcionController.php
@@ -28,12 +28,16 @@ class RecepcionController extends Controller
         $respuesta =  $this->data->respuesta;
 
         try {
-          
-            return $comensal_administrativo = DB::connection('mysql_third')->table('rrhh_personal_nomina')->where('pern_percodigo', 1)->first();
-            $comensal_administrativo = DB::connection('mysql_third')->table('rrhh_personal')->where('per_cedula', 24823972)->first();
 
-            $comensal_administrativo['data_nomina'] = DB::connection('mysql_third')->table('rrhh_personal_nomina')
-                ->where('pern_codigo', $comensal_administrativo->per_codigo)->first();
+            $comensal_administrativo = DB::connection('mysql_third')
+                ->table('rrhh_personal')
+                ->where('per_cedula', 24823972)
+                ->first();
+
+            $comensal_administrativo['data_nomina'] = DB::connection('mysql_third')
+                ->table('rrhh_personal_nomina')
+                ->where('pern_percodigo', $comensal_administrativo->per_codigo)
+                ->first();
 
             return $comensal_administrativo;
 
