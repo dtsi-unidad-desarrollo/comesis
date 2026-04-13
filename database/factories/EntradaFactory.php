53ea3e7ffe5abc5404a2fecf0f33065f6b8ed310
dtsi-unidad-desarrollo <dtsi.unidad.programadores@gmail.com>
2026-04-13 13:12:16 -0400

    feat(factory): define default state for Entrada model with faker data

--- database/factories/EntradaFactory.php
+++ database/factories/EntradaFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class EntradaFactory extends Factory
@@ -14,7 +15,23 @@ class EntradaFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'nombres' => $this->faker->firstName,
+            'apellidos' => $this->faker->lastName,
+            'nacionalidad' => 'V',
+            'cedula' => $this->faker->unique()->numberBetween(1000000, 99999999),
+            'sexo' => $this->faker->randomElement(['M', 'F']),
+            'comida' => $this->faker->randomElement(['ALMUERZO']),
+            'fecha' => Carbon::now()->format('Y-m-d'),
+            'hora' => Carbon::now()->format('H:i:s'),
+            'codigo_carrera' => $this->faker->randomElement([011, 010, 012, 013]),
+            'carrera' => $this->faker->jobTitle,
+            'codigo_sede' => $this->faker->randomElement([1, 2]),
+            'sede' => 'BARINAS',
+            'tipo_sede' => 'PRINCIPAL',
+            'estado' =>'BARINAS',
+            'municipio' => 'BARINAS',
+            'direccion' => 'BARINAS',
+            'tipo_comensal' => $this->faker->randomElement(['ESTUDIANTE', 'EMPLEADO']),
         ];
     }
 }
