bab34c43acc449f790b79452477b4fa53aa6e71d
dtsi-unidad-desarrollo <dtsi.unidad.programadores@gmail.com>
2026-05-20 15:53:17 -0400

    fix: mejorando la consulta de los empleados para obtener el tipo

--- app/Http/Controllers/RecepcionController.php
+++ app/Http/Controllers/RecepcionController.php
@@ -216,11 +216,12 @@ class RecepcionController extends Controller
             ->get();
         $comensal['rrhh_personal_cargo'] = DB::connection('mysql_third')
             ->table('rrhh_personal_cargo')
+            ->where('per_cedula', $cedula)
             ->get();
 
         $comensal['data'] = DB::connection('mysql_third')
             ->table('rrhh_personal')
-            ->where('per_cedula', $cedula)
+            ->where('perc_cedula', $comensal['data'][0]->per_codigo)
             ->get();
 
         $comensal['data_nomina'] = DB::connection('mysql_third')
