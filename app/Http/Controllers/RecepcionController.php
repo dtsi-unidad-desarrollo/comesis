ff5f0a57ffc975081a3a8a2a5ec19aad5d350125
dtsi-unidad-desarrollo <dtsi.unidad.programadores@gmail.com>
2026-05-20 15:25:47 -0400

    fix: mejorando la consulta de los empleados para obtener el tipo

--- app/Http/Controllers/RecepcionController.php
+++ app/Http/Controllers/RecepcionController.php
@@ -43,7 +43,7 @@ class RecepcionController extends Controller
             /** obtenemos el servicio actual activo por medio de la hora */
             $servicio = Helpers::getServicio($date);
 
-            return $comensal = $this->getEmpleados(25797040);
+            return $comensal = $this->getEmpleados(16791197);
 
             /** Obtenemos el total de entradas */
             if ($servicio) $cantidadDeEntradas = Helpers::getTotalEntradas($date->format('Y-m-d'), $servicio->nombre);
